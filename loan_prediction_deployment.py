801a10c27c4be146efb4b978521ce5fdb7a177de
Owais-arch <mohammedowaisbangi@gmail.com>
2026-03-14 16:44:02 +0530

    Update loan_prediction_deployment.py

--- loan_prediction_deployment.py
+++ loan_prediction_deployment.py
@@ -2,35 +2,32 @@ import streamlit as st
 import pickle
 import numpy as np
 
-from pathlib import Path
-import pickle
-
-model_path = Path(__file__).parent / "loan_model.pkl"
-
-with open(model_path, "rb") as file:
+# Load trained model
+with open("loan_model.pkl", "rb") as file:
     model = pickle.load(file)
+
 st.title("Loan Approval Prediction")
 
 ApplicantIncome = st.number_input("Applicant Income")
 CoapplicantIncome = st.number_input("Coapplicant Income")
 LoanAmount = st.number_input("Loan Amount")
 Loan_Amount_Term = st.number_input("Loan Amount Term")
-Credit_History = st.selectbox("Credit History",[0,1])
+Credit_History = st.selectbox("Credit History", [0,1])
 
-Gender = st.selectbox("Gender",["Male","Female"])
-Married = st.selectbox("Married",["Yes","No"])
-Education = st.selectbox("Education",["Graduate","Not Graduate"])
-Self_Employed = st.selectbox("Self Employed",["Yes","No"])
-Property_Area = st.selectbox("Property Area",["Urban","Semiurban","Rural"])
+Gender = st.selectbox("Gender", ["Male","Female"])
+Married = st.selectbox("Married", ["Yes","No"])
+Education = st.selectbox("Education", ["Graduate","Not Graduate"])
+Self_Employed = st.selectbox("Self Employed", ["Yes","No"])
+Property_Area = st.selectbox("Property Area", ["Urban","Semiurban","Rural"])
 
 if st.button("Predict"):
 
-    Gender_Male = 1 if Gender=="Male" else 0
-    Married_Yes = 1 if Married=="Yes" else 0
-    Education_Not_Graduate = 1 if Education=="Not Graduate" else 0
-    Self_Employed_Yes = 1 if Self_Employed=="Yes" else 0
-    Property_Area_Semiurban = 1 if Property_Area=="Semiurban" else 0
-    Property_Area_Urban = 1 if Property_Area=="Urban" else 0
+    Gender_Male = 1 if Gender == "Male" else 0
+    Married_Yes = 1 if Married == "Yes" else 0
+    Education_Not_Graduate = 1 if Education == "Not Graduate" else 0
+    Self_Employed_Yes = 1 if Self_Employed == "Yes" else 0
+    Property_Area_Semiurban = 1 if Property_Area == "Semiurban" else 0
+    Property_Area_Urban = 1 if Property_Area == "Urban" else 0
 
     input_data = np.array([[ApplicantIncome,
                             CoapplicantIncome,
@@ -47,12 +44,7 @@ if st.button("Predict"):
 
     prediction = model.predict(input_data)
 
-    if prediction[0]==1:
+    if prediction[0] == 1:
         st.success("Loan Approved")
     else:
         st.error("Loan Rejected")
-
-
-
-
-
